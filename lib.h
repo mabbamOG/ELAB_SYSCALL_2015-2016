6e3b13d7364197411cbf2a58b4218d145537cffa
M.A.B <mariobarbara@tutanota.de>
2016-06-28 00:28:24 +0200

    cleaned up code and moved in some functions from main file

--- lib.h
+++ lib.h
@@ -1,20 +1,17 @@
 void P(int semid, int semchoice)
 {
-    op->sem_num = semchoice;
-    op->sem_op = -1;
-    op->sem_flg = 0;
-    semop(semid, op, 1);
+    struct sembuf op = { semchoice, -1, 0 };
+    semop(semid, &op, 1);
 }
 
 
 void V(int semid, int semchoice)
 {
-    op->sem_num = semchoice;
-    op->sem_op = 1;
-    op->sem_flg = 0;
-    semop(semid, op, 1);
+    struct sembuf op = { semchoice, 1, 0 };
+    semop(semid, &op, 1);
 }
 
+// ???
 union semun  // needed for semctl() syscall
 {
    int              val;    /* Value for SETVAL */
@@ -25,6 +22,47 @@ union semun  // needed for semctl() syscall
 
 struct command
 {
-    char instruction;
-    int par1, par2, res;
+    char instr;
+    int par1, par2, res, res_pos;
 };
+
+int get_num_ops(char **buf_offset)
+{
+    char *s = *buf_offset;
+    int res = 0;
+    while (*s!='@')
+        if (*s++ == '\n')
+            ++res;
+    if (*(s-1) == '\n') // if last character of file is '\n', than i need to remove it
+        --res;
+    return res;
+}
+
+int next_integer(char **buf_offset)
+{
+    char *s = *buf_offset
+    while(*s== ' ' || *s=='\n')
+        ++s;
+    int sign = 1;
+    if (*s=='-')
+    {
+        sign = -1;
+        s++;
+    }
+    int res = 0;
+    while(*s>='0' && *s<='9')
+        res = res*10 + *s++ - '0';
+    *buf_offset = s;
+    return sign*res;
+}
+int next_command(char **buf_offset, struct command *cmd)
+{
+    int id = next_integer(buf_offset);
+    cmd->par1 = next_integer(buf_offset);
+    while(**buf_offset==' ')
+        ++*buf_offset;
+    cmd->instr = **buf_offset;
+    ++*buf_offset;
+    cmd->par2 = next_integer(buf_offset);
+    return id;
+}
