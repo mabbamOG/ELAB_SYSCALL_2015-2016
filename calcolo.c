ad98db1539850eff397e1b09ac7cc47e52d2a69d
M.A.B <mariobarbara@tutanota.de>
2016-06-28 00:29:48 +0200

    refactored code ALOT!

--- calcolo.c
+++ calcolo.c
@@ -1,72 +1,127 @@
-struct sembuf *op;
-struct command *CMDMEM;
+// TODO: standard naming of "get" and "next" functions, and all other functions!
+// TODO: check if i really need +1 size on both buf and semaphores and shmem
+// FIXME: is buf_offset really needed now that i keep whole file in memory and never go back?
+// TODO: split lib.h in multiple libraries containing different function types
+// TODO: ??? put global vars in lib.h?
+// TODO: finish father routine
+// TODO: find_free_process()
+// TODO: exception()
+#include "lib.h"
 
-int next_integer(char **buf_offset)
+// GLOBAL VARIABLES:
+#define MINFILESIZE 7
+#define MAXFILESIZE 10485760 // 10MB
+int SEMID_FREE = -1;
+int SEMID_WORK = -1;
+int SHMID = -1;
+struct command *SHM = (void *) -1;
+
+void exception(char *s)
 {
-    char *s = *buf_offset
-    while(*s== ' ')
-        ++s;
-    int sign = 1;
-    if (*s=='-')
-    {
-        sign = -1;
-        s++;
-    }
-    int res = 0;
-    while(*s>='0' && *s<='9')
-        res = res*10 + *s++ - '0';
-    *buf_offset = s;
-    return sign*res;
-}
-int next_command(char **buf_offset, struct command *cmd)
-{
-    while(**buf_offset==' ' || **buf_offset=='\n') // ????? should put this in next_integer?
-        ++*buf_offset;
-    int id = next_integer(buf_offset);
-    cmd->par1 = next_integer(buf_offset);
-    while(**buf_offset==' ')
-        ++*buf_offset;
-    cmd->instr = **buf_offset;
-    ++*buf_offset;
-    cmd->par2 = next_integer(buf_offset);
+    printf("%s\n",s);
+    printf("Freeing resources...\n");
+    printf("* Semaphore %d\n",SEMID_FREE);
+    if (SEMID_FREE != -1)
+        semctl(...);
+    printf("* Semaphore %d\n",SEMID_WORK);
+    if (SEMID_WORK != -1)
+        semctl(...);
+    if (SHM != (void *) -1)
+        shmdt(...);
+    printf("* Shared Memory %d\n",SHMID);
+    if (SHMID != -1)
+        shmctl(...);
+    printf("DONE!\n");
 }
 
-void slave(int id)
+void init_shared_resources(char *keystr,int res_size)
 {
-    while(1)
+    // Create First Semaphore Array
+    int key_t KEY = ftok(keystr,'a');
+        if (KEY == -1)
+            exception("ERROR while getting unique key for shared resource SEMID_FREE!");
+    SEMID_FREE = semget(KEY, res_size, IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG); // execute permission is meaningless for semaphores, but ok..
+        if (SEMID_FREE == -1)
+            exception("ERROR while getting id for shared resource SEMID_FREE!");
+        union semun cmdinfo;
+        unsigned short array[res_size] = {1};
+        cmdinfo.array = array;
+        if (semctl(SEMID_FREE, 0, SETALL, cmdinfo) == -1)
+            exception("ERROR while setting values for shared resource SEMID_FREE!");
+
+    // Create Second Semaphore Array
+    KEY = ftok(keystr,'b');
+        if (KEY == -1)
+            exception("ERROR while getting unique key for shared resource SEMID_WORK!");
+    int SEMID_WORK = semget(KEY, res_size, IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG); 
+        if (SEMID_WORK == -1)
+            exception("ERROR while getting id for shared resource SEMID_WORK!");
+        for (int i=0; i<res_size;++i)
+            cmdinfo.array[i] = 0;
+        if (semctl(SEMID_WORK, 0, SETALL, cmdinfo) == -1)
+            exception("ERROR while setting values for shared resource SEMID_WORK!");
+    
+    // Create and attach Shared Memory
+    KEY = ftok(keystr,'c'); // can be 'a'?
+        if (KEY == -1)
+            exception("ERROR while getting unique key for shared resource SHMID!");
+    int SHMID = shmget(KEY, sizeof(struct command)*res_size, IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG);
+        if (SHMID == -1)
+            exception("ERROR while getting id for shared resource SHMID!");
+    struct command *SHMEM = shmat(SHMID, NULL, 0);
+        if (SHMEM == (void *) -1)
+            exception("ERROR while attaching shared memory SHMID to process!");
+
+    // Initialize shared memory:
+    for(int i=0; i<res_size; ++i)
+        SHMEM[i].instr = '?'; // no op yet
+}
+
+
+void slave(int id)// ??? Should make get_data | drop result, more explicit?
+{
+    while(true)
     {
-        P(work,id);
-            switch(CMDMEM[id].instruction)// Get Data
+        P(SEMID_WORK,id);
+            switch(SHM[id].instr)// Get Data
             {
                 case '+':// Drop Result
-                    CMDMEM[id].res = CMDMEM[id].par1 + CMDMEM[id].par2;
+                    SHM[id].res = SHM[id].par1 + SHM[id].par2;
                     break;
                 case '-':
-                    CMDMEM[id].res = CMDMEM[id].par1 - CMDMEM[id].par2;
+                    SHM[id].res = SHM[id].par1 - SHM[id].par2;
                     break;
                 case '/':
-                    CMDMEM[id].res = CMDMEM[id].par1 / CMDMEM[id].par2;
+                    SHM[id].res = SHM[id].par1 / SHM[id].par2;
                     break;
                 case '*':
-                    CMDMEM[id].res = CMDMEM[id].par1 * CMDMEM[id].par2;
+                    SHM[id].res = SHM[id].par1 * SHM[id].par2;
+                    break;
+                case 'K':
+                    printf("PROC #%d: QUITTING!\n",id);
+                    exit(0);
                     break;
                 default:
+                    exception("invalid instruction found!");
                     break;
             }
-            CMDMEM[id].instruction = '!';// ??? needed?
-        V(free,id);
+            SHM[id].instr = '!'; // telling master i have ever run any op
+        V(SEMID_FREE,id);
     }
 }
 
-void master(int id)
+void master(int id, struct command *cmd, int *RESULTS)
 {
-    P(free,id);
-    // Get result...
-    if (CMDMEM[id] == '!')
-        RESULTS[
-    // Drop the data
-    // ...
-    V(work,id);
+    P(SEMID_FREE,id);
+        // Get result...
+        if (SHM[id].instr == '!') // making sure this isn't the first instruction
+            RESULTS[SHM[id].res_pos] = SHM[id].res;
+        // Drop the data
+        SHM[id].res_pos = cmd->res_pos;
+        SHM[id].instr = cmd->instr;
+        SHM[id].par1 = cmd->par1;
+        SHM[id].par2 = cmd->par2;
+    V(SEMID_WORK,id);
 }
 
 void help()
@@ -86,74 +141,65 @@ int main(int argc, char **argv)
     if (argc != 3)
         help();
 
-    // Init resources:
-    int finput = open(argv[1], O_RDONLY, S_IRUSR | S_IRGRP);
-    int foutput = open(argv[2], O_WRONLY | O_CREAT | O_SYNC, S_IWUSR | S_IWGRP);
+    // Get access to input and output files:
+    int fdinput = open(argv[1], O_RDONLY, S_IRUSR | S_IRGRP);
+    if (fdinput == -1)
+        exception("ERROR while opening input file!");
+    int fdoutput = open(argv[2], O_WRONLY | O_CREAT | O_SYNC, S_IWUSR | S_IWGRP);
+    if (fdoutput == -1)
+        exception("ERROR while opening output file!");
 
     // Get Input filesize:
-    const int FILESIZE;
-    struct stat *stats = malloc(sizeof(struct stat));
-    fstat(finput, stats);
-    FILESIZE = stats->st_size;
-    free(stats);
+    struct stat stats;
+    if (fstat(finput, &stats) == -1)
+        exception("ERROR while getting input file filesize!");
+    const int FILESIZE = stats->st_size + 1; // !!! +1 because i need space for end marker!
+    if (FILESIZE < MINFILESIZE + 1)
+        exception("ERROR input file too small");
+    if (FILESIZE > MAXFILESIZE + 1)
+        exception("ERROR input file way too big");
 
     // Read file into buffer:
     char buf[FILESIZE];
     char *buf_offset = buf;
-    int nread = read(finput, buf, FILESIZE)
+    if(read(finput, buf, FILESIZE) == -1)
+        exception("ERROR while loading input file into buffer!");
+    buf[FILESIZE-1]='@';
+    if (close(fdinput) == -1)
+        exception("ERROR while closing input file!");
 
     // Get threads:
     printf("Getting number of threads...\n");
-    int NPROCS = next_number(&buf_offset);
+    const int NPROCS = next_number(&buf_offset);
     if (NPROCS<1)
         exception("please create more processes");
 
     //  Init shared resources:
-    //  !!! using NPROCS+1 because i want to use the id to address a semaphore or shared memory, and the id is >=1 in the input file! (clarity process)
-    int key_t KEY = ftok(argv[2],'a');
-    int semfreeid = semget(KEY, NPROCS+1, IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG); // execute permission is meaningless for semaphores, but ok..
-        union semun cmdinfo;
-        unsigned short array[NPROCS+1] = {1};
-        cmdinfo.array = array;
-        semctl(semfree, 0, SETALL, cmdinfo);
-    KEY = ftok(argv[2],'b');
-    int semworkid = semget(KEY, NPROCS+1, IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG); 
-        for (int i=0; i<NPROCS+1;++i)
-            cmdinfo.array[i] = 0;
-        semctl(semfree, 0, SETALL, cmdinfo);
-    KEY = ftok(argv[2],'c'); // can be 'a'?
-    int shmid = shmget(KEY, sizeof(struct command)*(NPROCS+1), IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG);
-        struct command *CMDMEM = shmat(shmid, NULL, 0);
+    init_shared_resources(NPROCS + 1); // !!! +1 because i want to use "id" freely
 
     // Make threads:
     for (int id = 1; id<=NPROCS; ++id)
     {
         int pid = fork();
-        if (pid<0)
-            exception("one of the forks failed");
+        if (pid == -1)
+            exception("ERROR while forking!");
         else if (pid==0)
             slave(id);
     }
 
     // Father:
-    struct command *cmd = malloc(sizeof(struct command));
-    do
+    struct command cmd;
+    const int NOPS = get_num_ops(buf_offset);
+    int RESULTS[NOPS];
+    for (int j = 0; j<NOPS; ++j)
     {
-        int NOPS = get_num_ops(buf_offset);
-        int RESULTS[NOPS]
-        for (int j = 0; j<NOPS; ++j)
-        {
-            int id = next_command(buf_offset,cmd);
-            if (id == 0)
-            {
-                // DO OPS TO FIND FREE SEMAPHORE
-                //....
-            }
-            master(id,j,RESULTS);
-        }
-            
-    } while( (nread=read(finput, buf, BUFSIZE)) >0);
-    if (nread==-1)
-        exception("error reading file");
+        // Setup info to send to process:
+        cmd.res_pos = j;
+        int id = next_command(buf_offset,&cmd);
+        if (id == 0)
+            id = find_free_process();
 
+        // Send Off Command (possibly grabbing result for previously sent command)
+        master(id, &cmd, RESULTS);
+    }
 }
