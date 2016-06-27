d80d80dd9dfe2d61c728cd4a8ac854a46c8f8608
M.A.B <mariobarbara@tutanota.de>
2016-06-27 11:53:20 +0200

    moved some structs from main file to library file

--- lib.h
+++ lib.h
@@ -14,3 +14,17 @@ void V(int semid, int semchoice)
     op->sem_flg = 0;
     semop(semid, op, 1);
 }
+
+union semun  // needed for semctl() syscall
+{
+   int              val;    /* Value for SETVAL */
+   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+   unsigned short  *array;  /* Array for GETALL, SETALL */
+   struct seminfo  *__buf;  /* Buffer for IPC_INFO, Linux-Specific*/
+};
+
+struct command
+{
+    char instruction;
+    int par1, par2, res;
+};
